05415f2e136c531b372b824d126e7dfd3872c90f
seungkyu2 <zx6658@naver.com>
2018-02-21 02:54:46 +0900

    user모델에 devise gem 추가(teamid와 name받아서 회원가입하게)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209180007) do
+ActiveRecord::Schema.define(version: 20180220171153) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "email"
@@ -40,10 +40,21 @@ ActiveRecord::Schema.define(version: 20180209180007) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.integer "team_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["team_id"], name: "index_users_on_team_id"
   end
 
