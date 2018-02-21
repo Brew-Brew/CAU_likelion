38e2998f36f37191346bf3129be6b9c245d211f7
seungkyu2 <zx6658@naver.com>
2018-02-22 00:21:30 +0900

    권한부여 부분추가, 및 기타 수정.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
   belongs_to :team
+
+
 end
