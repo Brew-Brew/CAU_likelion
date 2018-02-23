c0b25c3a31859938b8e03b43225d949ce420fb6b
seungkyu2 <zx6658@naver.com>
2018-02-23 20:58:32 +0900

    glyphicon 되게하

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Caulion
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
