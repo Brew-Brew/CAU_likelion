1613c2b7ec6ba9842f1e5c509802a267532043aa
seungkyu2 <zx6658@naver.com>
2018-02-28 01:11:01 +0900

    comment 권한여부에따라 지우게 하기.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,29 @@
 class CommentsController < ApplicationController
-  
+
   #before_action :authenticate_user!
   #before_action :authenticate_user!, only: [:destroy]
-  
+
   def create
     if user_signed_in?
       @post = Post.find(params[:post_id])
-      @comment = @post.comments.create!(params.require(:comment).permit!) 
+      @comment = @post.comments.create!(params.require(:comment).permit!)
       @comment.commenter = current_user.name
       @comment.save
       redirect_to post_path(@post.team_id,@post)
     end
   end
-  
+
   def destroy
     if user_signed_in?
       @post = Post.find(params[:post_id])
       @comment = @post.comments.find(params[:id])
+      if @comment.commenter != current_user.name
+        flash.now[:alert] = '본인만 이글을 삭제할 수있습니다!'
+        redirect_back(fallback_location: root_path)
+      else
       @comment.destroy
       redirect_to post_path(@post.team_id,@post)
+    end
     end
   end
 end
