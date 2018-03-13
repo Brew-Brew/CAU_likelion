a50f239832f3921fa889b4c12c40d31808727f08
seungkyu2 <zx6658@naver.com>
2018-03-14 03:21:34 +0900

    edit env file to reslove not showing css,image in aws.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,6 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-  config.serve_static_assets = false
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
