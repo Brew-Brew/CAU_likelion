9eb64d1ea3611ebda9e6f9502493bc024e4050e5
seungkyu2 <zx6658@naver.com>
2018-03-14 22:47:52 +0900

    fix error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   layout "application"
-  protect_from_forgery with: :exception, prepend: true
-   skip_before_action :verify_authenticity_token ,raise: false
+
   #아래 부분은 devise 에 name,teamid를 추가하여 등록하기 위해 추가된 부분이다.
   # before_action :configure_permitted_parameters, if: :devise_controller?
   # protected
