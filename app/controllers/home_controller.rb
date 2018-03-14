bc0057f5f3b3cd51d14681ec93cb500ec5d61e05
seungkyu2 <zx6658@naver.com>
2018-03-14 22:20:51 +0900

    solve can't login in production final.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   protect_from_forgery
-
   def index
   end
 
--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User::RegistrationsController < Devise::RegistrationsController
-  protect_from_forgery prepend: true
+  protect_from_forgery
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
