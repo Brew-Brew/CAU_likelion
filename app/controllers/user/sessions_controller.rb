07b5d7bc6cec1082360848de3a85169346c587c0
seungkyu2 <zx6658@naver.com>
2018-03-14 23:14:20 +0900

    fix login..

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User::SessionsController < Devise::SessionsController
+  protect_from_forgery
   # protect_from_forgery
   # before_action :configure_sign_in_params, only: [:create]
   #
