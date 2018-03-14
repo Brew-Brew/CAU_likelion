603f8162721af0d0c836fa07b6fc8939995b65f3
seungkyu2 <zx6658@naver.com>
2018-03-14 22:50:57 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   layout "application"
-
+  protect_from_forgery 
   #아래 부분은 devise 에 name,teamid를 추가하여 등록하기 위해 추가된 부분이다.
   # before_action :configure_permitted_parameters, if: :devise_controller?
   # protected
