d10ef6582520735b81dc71483ff697329c892c3a
seungkyu2 <zx6658@naver.com>
2018-03-18 19:09:43 +0900

    gitignore edit and deal with error.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( team.js )
 Rails.application.config.assets.precompile += %w( custom.css )
-Rails.application.config.assets.precompile += %w( home.scss )
+Rails.application.config.assets.precompile += %w( home.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
